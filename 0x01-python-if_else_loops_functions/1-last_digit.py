e0788c0a84a10c64ca82619461fd2c3892d211f4
kchinyama <kaumba@kalichipictures.com>
2023-03-08 08:46:21 -0800

    Task 2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 5:
+    print("Last digit of {} is {} and is greater than 5".format("random", "number"))
+elif number == 0:
+    print("Last digit of {} is {} and is 0".format("random", "number"))
+elif number < 6:
+    else
