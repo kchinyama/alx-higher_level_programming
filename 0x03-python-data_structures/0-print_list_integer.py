94b9f6591b4a54b9bca8d6a8f40721fc1332190e
kchinyama <kaumba@kalichipictures.com>
2023-03-13 03:04:13 -0700

    Task 0. Print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-__name__ == "__main__"
 def print_list_integer(my_list=[]):
-    for k in print_list_integer:
-        print("{}".format(print_list_integer))
+    for k in my_list:
+        print("{}".format(k))
