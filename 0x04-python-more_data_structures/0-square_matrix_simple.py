ca27e473fb155bfce1acb0cbcb68e0cf2b4b932e
kchinyama <kaumba@kalichipictures.com>
2023-03-15 07:53:56 -0700

    Task 0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    square = lambda k: k**2
-    for k in matrix:
-        new_matrix.append(square(k))
-    return new_matrix
+    if not matrix:
+        print()
+    return [[k**2 for k in row] for row in matrix]
