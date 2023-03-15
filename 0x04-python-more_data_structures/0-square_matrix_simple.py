2714352e373e8bedd13e2366f4b76a84b264f7e6
kchinyama <kaumba@kalichipictures.com>
2023-03-15 07:49:14 -0700

    Task 0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda k: k**2, matrix))
+    new_matrix = []
+    square = lambda k: k**2
+    for k in matrix:
+        new_matrix.append(square(k))
     return new_matrix
