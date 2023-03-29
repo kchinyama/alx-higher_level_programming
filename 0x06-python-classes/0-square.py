4a9c36d6a70baf734f2b3dc40d410e6247b6ee52
kchinyama <kaumba@kalichipictures.com>
2023-03-29 02:04:32 -0700

    Task 0.My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-"0-square.py definition of a square"
+'''python3 -c 'print(__import__("my_module").MyClass.__doc__)'''
 class Square:
+    '''python3 -c 'print(__import__("my_module")
+    .MyClass.my_function.__doc__)'''
     pass
