14f6ae1cd94d0fdde1eebf7a6856246b0e8abaea
kchinyama <kaumba@kalichipictures.com>
2023-03-29 02:54:43 -0700

    Task 3. Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''python3 -c 'print(__import__("my_module").MyClass.__doc__)'''
+class Square:
+    '''python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'''
+    def __init__(self, size=0):
+        '''size initialised to 0 as instructed'''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    '''python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'''
+    def area(self):
+        '''definitions will follow here'''
+        return self.__size ** 2
