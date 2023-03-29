f6783b72d40faa7a2ea0316c91447689aaef2ae7
kchinyama <kaumba@kalichipictures.com>
2023-03-29 08:24:21 -0700

    Task 5. Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''python3 -c 'print(__import__("my_module").MyClass.__doc__)'''
+class Square:
+    '''python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'''
+    def __init__(self, size=0):
+        self.size = size
+    
+    '''getter for retrieving and presenting the data with more user appeal'''
+    @property
+    def size(self):
+        return self.__size
+
+    '''set setter in order to clean out the output'''
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    '''python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'''
+    def area(self):
+        '''definitions will follow here'''
+        return self.__size ** 2
+    
+    def my_print(self):
+        print("#",self.__size ** 2)
+        if size == 0:
+            print("--")
