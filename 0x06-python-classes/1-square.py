7a4b67e9a0934e8a06ca1b04fac8f8cbd86444ea
kchinyama <kaumba@kalichipictures.com>
2023-03-29 02:12:37 -0700

    Task 1.Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''python3 -c 'print(__import__("my_module").MyClass.__doc__)'''
+class Square:
+    '''python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'''
+    def __init__(self, size=None):
+        '''size initialised to None but will be updated subsequently'''
+        pass
