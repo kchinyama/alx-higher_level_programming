66ae0d33be6d51e02b9c9f9fc83d4b4df135e94c
kchinyama <kaumba@kalichipictures.com>
2023-03-29 02:43:46 -0700

    Task 2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,12 +4,10 @@ class Square:
     '''python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'''
     def __init__(self, size=0):
         '''size initialised to 0 as instructed'''
-        self.__size = size
-        try:
-            size > 0
-        except TypeError:
-            print("size must be an integer")
-        try:
-            size < 0
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        
