39dd092ec10e5cf7132a7d5c761f88c54dd09290
kchinyama <kaumba@kalichipictures.com>
2023-03-29 08:04:28 -0700

    Task 4. Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,10 +18,10 @@ class Square:
     '''set setter in order to clean out the output'''
     @size.setter
     def size(self, value):
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
