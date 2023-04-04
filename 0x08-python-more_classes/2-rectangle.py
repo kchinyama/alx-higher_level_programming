07138dae0edeb3aa5a10254240c9ce07ba81b17e
kchinyama <kaumba@kalichipictures.com>
2023-04-04 03:31:22 -0700

    Task 2. Area and Perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+""" declaration of class rectangle
+"""
+class Rectangle:
+    """initialize init method for ease of copying and pasting subsequent methods
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    """initialize width method as instructed-getter.
+    """
+    @property
+    def width(self):
+        """Returns the width
+        """
+        return self.__width
+
+    """initialize the width method as instructed, we want to set it
+    """
+    @width.setter
+    #    """Protect it from bad values
+    #    """
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    """initialize height method as instructed-getter.
+    """
+    @property
+    def height(self):
+        """Returns the height
+        """
+        return self.__height
+
+    """initialize the height method as instructed, we want to set it
+    """
+    @height.setter
+    #"""Protect value from bad input values
+    #"""
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value <= 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+        """initialize public method area as instructed
+        """
+    def area(self):
+        """ area is height x width. Returns computed number
+        """
+        return width * height
+
+    def perimeter(self):
+        """ perimeter is the distance around the circumfrance of the shape
+        """
+        if width == 0 or height == 0:
+            return 0
+        """ computed perimeter when width and length values are not zero
+        """
+        return (width * 2) + (height * 2)
