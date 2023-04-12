09c8492f4786f458dd02e0db467d5d51a81cba75
kchinyama <kaumba@kalichipictures.com>
2023-04-12 13:09:38 -0700

    Task 0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''this is a module for reading a file in python
+'''
 def read_file(filename=""):
-    with open("filename", 'r', encoding="utf-8") as read_file:
-        print(read_file.read())
+    '''reading a file using the 'with' method
+    '''
+    with open("filename", 'r', encoding="utf-8") as my_file:
+        print(my_file.read())
