c85817d12c3a32fcc85ea104f56bc0ca4b8fcbb0
kchinyama <kaumba@kalichipictures.com>
2023-04-12 14:13:29 -0700

    Task 3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''function returns the JSON representation of an object
+'''
+def to_json_string(my_obj):
+    '''json representation of an object
+    '''
+    json.dumps(my_obj)
