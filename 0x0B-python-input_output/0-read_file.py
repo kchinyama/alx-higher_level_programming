4744c74b4f8fbc6c5c4be1f4aab6a244c87908e7
kchinyama <kaumba@kalichipictures.com>
2023-04-12 12:57:13 -0700

    Task 0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open("filename", mode='r', encoding="utf-8") as read_file:
-        print(read_file.read(), end="")
+    with open("filename", 'r', encoding="utf-8") as read_file:
+        print(read_file.read())
