3b476f85c3330bd73b509bf37348ce32bd400a32
kchinyama <kaumba@kalichipictures.com>
2023-04-12 13:17:30 -0700

    Task 1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''function writes a string and returns the number
+of characters
+'''
+
+def write_file(filename="", text=""):
+    '''write file on a file, function overwrites
+    previous contents if file already exists
+    '''
+    with open("filename", mode='w', encoding="utf-8") as my_file:
+        my_file.text("This School is so cool!")
