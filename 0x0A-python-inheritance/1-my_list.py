bda326c1aa3731bf7991f9323f4833b9d75a7e5d
kchinyama <kaumba@kalichipictures.com>
2023-04-13 08:34:47 -0700

    Task 1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''module print_sorted
+class MyList
+'''
+
+class MyList(list):
+
+    def print_sorted(self):
+        '''function prints the list in ascending sort order
+        '''
+        print(sorted(self))
