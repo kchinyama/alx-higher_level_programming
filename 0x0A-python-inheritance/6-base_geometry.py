a413a56317a5763bdc8304dedb10104afca85b80
kchinyama <kaumba@kalichipictures.com>
2023-04-13 11:37:13 -0700

    Task 6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''My empty class
+'''
+
+class BaseGeometry:
+    '''raises Exception with the message'''
+    def area(self):
+        '''like this'''
+        raise Exception("area() is not implemented")
