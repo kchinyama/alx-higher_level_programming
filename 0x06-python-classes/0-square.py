3e8b3050c3b7f84ac29bd148d283aed0ef80f939
kchinyama <117774472+kchinyama@users.noreply.github.com>
2023-07-05 11:38:14 +0200

    Delete 0x06-python-classes directory

--- 0x06-python-classes/0-square.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-'''python3 -c 'print(__import__("my_module").MyClass.__doc__)'''
-class Square:
-    '''python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'''
-    pass
--- 0x06-python-classes/1-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-'''python3 -c 'print(__import__("my_module").MyClass.__doc__)'''
-class Square:
-    '''python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'''
-    def __init__(self, size=None):
-        '''size initialised to None but will be updated subsequently'''
-        self.__size = size
