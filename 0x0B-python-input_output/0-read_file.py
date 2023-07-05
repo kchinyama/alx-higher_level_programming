9a37868597cbd77db8daff7c60425f8fc2d740bf
kchinyama <117774472+kchinyama@users.noreply.github.com>
2023-07-05 11:39:41 +0200

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-'''this is a module for reading a file in python
-'''
-def read_file(filename=""):
-    '''reading a file using the 'with' method
-    '''
-    with open("filename", 'r', encoding="utf-8") as my_file:
-        print(my_file.read())
