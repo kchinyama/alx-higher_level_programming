ddd8bdcc5d9dc698a546a3154dd568912c752b1d
kchinyama <117774472+kchinyama@users.noreply.github.com>
2023-07-05 11:37:21 +0200

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/9-max_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def max_integer(my_list=[]):
-    if not my_list:
-        return None
-    biggest_int = 0
-    for k in my_list:
-        if k > biggest_int:
-            biggest_int = k
-    return biggest_int
