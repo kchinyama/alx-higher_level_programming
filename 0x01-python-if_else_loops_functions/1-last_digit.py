9a982e144baab890646584f503148d552aa48ee7
root <kaumba@kalichipictures.com>
2023-08-14 10:18:18 -0700

    task 7

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number)
+if abs(number) % 10 > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, number[-1]))
+elif abs(number) % 10 == 0:
+    print("Last digit of {} is {} and is 0".format(number, number[-1]))
+elif abs(number) % 10 < 6 and number != 0:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, number[-1]))
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def islower(c):
+    if c == ord(range(97, 123)):
+        return True
+    else:
+        return Fales
