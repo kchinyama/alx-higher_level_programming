28be0f2cf669108ced8b1f2ebba51879a756ec22
root <kaumba@kalichipictures.com>
2023-08-15 14:27:11 -0700

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    for k in number:
-        if k == int(chr(number)[-1]):
-            print(k)
-    #print(int(chr(number)[-1]))
+    print(int(chr(number)[-1]))
