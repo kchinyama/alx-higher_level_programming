2556ea20794389fd9aa15c5e8f98fc950a053bc7
root <kaumba@kalichipictures.com>
2023-08-20 10:41:47 -0700

    task 8

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for k in range(101):
+    for k in range(1, 101):
         if k % 3 == 0:
             print("Fizz ", end="")
         elif k % 5 == 0:
             print("Buzz ", end="")
-        elif (k % 3 == 0) and (k % 5 == 0):
+        elif k % 3 == 0 and k % 5 == 0:
             print("Fizz Buzz ", end="")
         else:
-            print("{} ".format(k, end=""))
+            print("{} ".format(k), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for k in str:
-        if ord(k) 
+        if ord(k) > 96 and ord(k) < 123:
+            k = chr(ord(k) - 32)
+        print("{}".format(k), end="")
+    print("")
