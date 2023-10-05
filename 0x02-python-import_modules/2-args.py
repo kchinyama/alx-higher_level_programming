55fe03cf1d26de8bd20d46e0e5951ec2ec6546e7
root <kaumba@kalichipictures.com>
2023-10-05 12:25:50 -0700

    typos on task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 # variable to hold the number of arguments
-n = len(sys.argv) - 1
+    n = len(sys.argv) - 1
 
-if n == 0:
-    print("{} arguments.".format(n))
-elif n == 1:
-    print("{} argument:".format(n))
-else:
-    print("{} arguments:".format(n))
+    if n == 0:
+        print("{} arguments.".format(n))
+    elif n == 1:
+        print("{} argument:".format(n))
+    else:
+        print("{} arguments:".format(n))
 
-if n >= 1:
-    n = 0
-    for arg in sys.argv:
-        if n != 0:
-            print("{}: {}".format(n, arg))
-        n += 1
+    if n >= 1:
+        n = 0
+        for arg in sys.argv:
+            if n != 0:
+                print("{}: {}".format(n, arg))
+            n += 1
