7a00c2be7fc037842973d64c7ec8b41d7fa3b0ce
root <kaumba@kalichipictures.com>
2023-10-09 03:35:38 -0700

    task 3

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for k in my_list:
+        if k < 0:
+            return None
+#        if my_list[idx] > :
+
+        else:
+            return my_list[k]
