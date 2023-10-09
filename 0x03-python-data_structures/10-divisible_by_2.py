c85bedd649838cfd7fcbed383eb03fe6f328b6c6
root <kaumba@kalichipictures.com>
2023-10-09 13:48:11 -0700

    task 8

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for k in my_list:
+        if k % 2 == 0 or if k % 2 == 1:
+            return my_list
+        else:
+            return my_list
