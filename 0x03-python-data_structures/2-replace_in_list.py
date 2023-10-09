25e38b8ab090e0a82bdaca0009fcf84b17d5ecb4
root <kaumba@kalichipictures.com>
2023-10-09 10:58:35 -0700

    task 12

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    new_list = []
+    for idx in my_list:
+        if idx < 0:
+            return my_list
+        elif idx > len(my_list):
+            return my_list
+        else:
+            return my_list
