4a0eb54ec9eebae6a7b956bc9defa342175fefd2
root <kaumba@kalichipictures.com>
2023-10-23 13:31:11 -0700

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = a / b
-try:
-
+    try:
+        result = a/b
+    except:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
