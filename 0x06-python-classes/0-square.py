73a59ac0ce1096c68424609917614eb6e7d571ca
root <kaumba@kalichipictures.com>
2023-10-24 12:10:00 -0700

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class Square:
+    """creation of a square class-all instances will be of this class"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This directory contains my completed Tasks in the alx 0x06. Python - Classes and Objects Project
