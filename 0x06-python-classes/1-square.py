268b364691cd6d6383d8dcfbb78664b06e221d76
root <kaumba@kalichipictures.com>
2023-10-24 13:16:48 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+this is a square class
+"""
+class Square:
+    """body of class to follow"""
+    def __init__(self, size):
+        """initialise the square"""
+        self.__size = size
