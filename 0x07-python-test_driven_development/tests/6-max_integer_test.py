5a0f19b99e8d1b7aa1ef1ea5af44c47e62bf4d0a
root <kaumba@kalichipictures.com>
2023-11-03 09:22:01 -0700

    Task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
 
 import unittest
 
 max_integer = __import__('6-max_integer').max_integer
 
-class Test_Max_Integer(unittest.Testcase)
+class Test_Max_Integer(unittest.TestCase):
+    """first we can test for correct output"""
+    def test_norm(self):
+        self.assertEqual(max_integer([2,4,6,8]), 8)
+        self.assertEqual(max_integer([-2, -6, -3, -9]), -2)
+        self.assertEqual(max_integer([10]), 10)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
