f106c8e08da2f4a1f362968bcfd9e35de06e9314
root <kaumba@kalichipictures.com>
2023-11-06 08:38:30 -0800

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""demo of class in alx
+"""
+
+
+class BaseGeometry:
+    """public method area"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.__name = name
+        self.__value = value
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        return value
