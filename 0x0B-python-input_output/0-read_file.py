839c72253ab4318be3b7e18ca2994ff50cdbbfb3
root <kaumba@kalichipictures.com>
2023-11-07 04:42:58 -0800

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module demo' reads and prints to stdout
+"""
+
+
+def read_file(filename=""):
+    """body of function"""
+
+    with open(filename, "r", encoding="utf-8") as k:
+        print(k.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This directory contians my completed tasks inside the alx 0x0B. Python - Input/Output project
