50228dc08d758d3eaa0a58f9e6c73eac7b6ad938
root <kaumba@kalichipictures.com>
2023-11-07 06:23:48 -0800

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """module that demo' JSON rep of str
 """
-
-
 import json
 
 
 def to_json_string(my_obj):
-    return json.dumps(my_obj)
+    """convert obj to json format"""
+    return (json.dumps(my_obj))
