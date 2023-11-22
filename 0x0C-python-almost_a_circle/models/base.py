71eba5afa12d3e30d16db1096b35cb308a8c4b13
root <kaumba@kalichipictures.com>
2023-11-22 00:35:31 -0800

    null

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@
 
 
 import json
+#import csv
+
 
 class Base:
     """the base class"""
@@ -26,10 +28,66 @@ class Base:
         else:
             return json.dumps(list_dictionaries)
 
+
     @classmethod
     def save_to_file(cls, list_objs):
-        """class method that writes json str rep of class objects"""
+        """writes json string rep of list_objs to a file"""
+        file_name = cls.__name__ + ".json"
 
-        for k in list_objs:
-            with open(k, 'w', encoding='utf-8') as new_file:
-                json.dumps(new_file)
+        json_list = []
+
+        if list_objs is not None:
+            json_list = [obj.to_dictionary() for obj in list_objs] #convert each to dictionary
+
+            json_string = cls.to_json_string(json_list) #json string rep of the list
+
+            with open(file_name, 'w', encoding='utf-8') as f:
+                f.write(json_string) #open and write to the file
+
+
+    @staticmethod
+    def from_json_string(json_string):
+        """method returns the list of the json string rep of json_string"""
+
+        if json_string is None or len(json_string) == 0:
+            return []
+
+        else:
+            return json.loads(json_string)
+
+
+    @classmethod
+    def create(cls, **dictionary):
+        """method auto updates attributes to an instance"""
+
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 2)
+
+        if cls.__name__ == "Square":
+            dummy = cls(2)
+
+        dummy.update(**dictionary) # update dummy attribute values with those from dict
+
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns list of instances from a file"""
+
+        file_name = cls.__name__ + 'json'
+
+        try:
+            with open(file_name, 'r', encoding='utf-8') as f:
+                info = f.read() # read content in file
+
+                list_dict = cls.from_json_string(info) # convert json string to list of dictionaries
+
+                list_ins = [] # variable to hold list of instances
+
+                for k in list_dict: # iterate through the dictioneries
+                    list_ins.append(cls.create(**k)) #create instance using built in create method
+
+                return list_ins # return the list of instances
+
+        except FileNotFoundError: # if file is no existant
+            return [] # empty list
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -103,6 +103,6 @@ class Rectangle(Base):
         """setter for y"""
         if type(value) is not int:
             raise TypeError("y must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
