003f2cf04adce3e75831ad7e63545314b1de3117
root <kaumba@kalichipictures.com>
2023-12-12 13:39:47 -0800

    task 15-desc edit

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists number of records with same score
-SELECT score, COUNT(*) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY score DESC;
