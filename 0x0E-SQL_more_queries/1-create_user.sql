68f69c6cf9dd84d7afc1f70c5b342b007f8cf659
root <kaumba@kalichipictures.com>
2023-12-13 13:16:05 -0800

    task 1-edits

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- demo of creating a new database user
 -- granting all privileges to this ne user via password
 CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1@localhost';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
