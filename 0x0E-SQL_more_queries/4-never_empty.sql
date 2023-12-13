88d989dbed94ac166d3e150985ac89a83918b5be
root <kaumba@kalichipictures.com>
2023-12-13 13:29:38 -0800

    Task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- demo showing creation of table
-CREATE TABLE IF NOT EXISTS id_not_null (id=1 INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
