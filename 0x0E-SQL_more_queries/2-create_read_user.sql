cf6322bbc6ea50068792cc9d1d6b24047c8d7715
root <kaumba@kalichipictures.com>
2023-12-14 10:11:39 -0800

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- demo of creation of database and user
 -- new user must have select priviledge only
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2@localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT, INSERT ON hbtn_0d_2.* TO 'user_0d_2@localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT, INSERT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
