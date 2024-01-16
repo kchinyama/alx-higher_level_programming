8169dd6c0d073f00cd23432b6ae4dcfb502e7d4b
root <kaumba@kalichipictures.com>
2024-01-16 04:24:34 -0800

    task 0 mune

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
+
 """
 demo script that displays all cities in database
 """
 
+import MySQLdb
+import sys
+
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+    """the arguments needed for the query"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
     db = MySQLdb.connect(
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database)
 
+    """create cursor object to navigate the database and tables"""
     mycur = db.cursor()
 
-    mycur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
-    rows = mycur.fetchall()
+    """use cursor object to execute command to list all states in database"""
+    mycur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-    for k in rows:
-        print(k)
+    """use loop to display results"""
+    queried_items = mycur.fetchall()
+
+    for state in queried_items:
+        print(state)
 
     mycur.close()
     db.close()
