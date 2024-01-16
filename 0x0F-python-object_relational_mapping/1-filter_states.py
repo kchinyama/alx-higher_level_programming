c96bae0650bb6963f68e3575659005c720a7f56a
root <kaumba@kalichipictures.com>
2024-01-16 04:51:52 -0800

    task 1 -indent

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-demo script that displays all cities with name 
+demo script that displays all cities with name
 starting "N" in database
 """
 
@@ -18,7 +18,9 @@ if __name__ == "__main__":
     mycur = db.cursor()
 
     """use cursor object to execute command to list all states in database"""
-    mycur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    mycur.execute("SELECT * FROM states
+                  WHERE name LIKE 'N%'
+                  ORDER BY states.id ASC")
 
     """use loop to display results"""
     queried_items = mycur.fetchall()
