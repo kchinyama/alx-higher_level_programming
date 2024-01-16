fd04cec8ad0356e3672c8aaa83ae4079bb7a5adc
root <kaumba@kalichipictures.com>
2024-01-16 05:00:52 -0800

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+demo script that displaysname of city typed to input
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    """the arguments needed for the query"""
+    db = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+        state_name = argv[4])
+
+    """create cursor object to navigate the database and tables"""
+    mycur = db.cursor()
+
+    """use cursor object to execute command to list all states in database"""
+    mycur.execute("SELECT * FROM states WHERE name LIKE BINARY {} ORDER BY states.id",format(state_name))
+
+    """use loop to display results"""
+    queried_items = mycur.fetchall()
+
+    for state in queried_items:
+        print(state)
+
+    mycur.close()
+    db.close()
