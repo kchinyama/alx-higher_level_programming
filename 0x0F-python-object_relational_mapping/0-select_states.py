4b814fb7ebe3472b6594356b061c4b304c35bbf3
root <kaumba@kalichipictures.com>
2024-01-16 03:15:48 -0800

    comp edits

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     mycur = db.cursor()
 
     """use cursor object to execute command to list all states in database"""
-    mycur.execute("SELECT * FROM states ORDER BY id ASC")
+    mycur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
     """use loop to display results"""
     queried_items = mycur.fetchall()
