9bfd1f463dbf395a44a8f51e165d9eae79757571
root <kaumba@kalichipictures.com>
2024-01-16 03:50:43 -0800

    task 0-finally

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,6 @@ if __name__ == "__main__":
     from sys import argv
 
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
         user=argv[1],
         passwd=argv[2],
         db_name=argv[3])
