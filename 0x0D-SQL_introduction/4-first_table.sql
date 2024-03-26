3cc446c02bd7878435a6bf4a9bfa6b19cbdf6068
root <kaumba@kalichipictures.com>
2024-03-26 08:48:36 -0700

    task 4, had to recreate the database

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- creates a table first_table in mysql
 -- CREATE DATABASE hbtn_0c_0
-CREATE TABLE IF NOT EXISTS 'first_table'(id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
