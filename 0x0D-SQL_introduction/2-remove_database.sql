7436745b46f25765dae6fcba1222edd5f2d7a795
root <kaumba@kalichipictures.com>
2024-03-26 07:53:19 -0700

    task 2 edits

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- creates databases hbtn_0c_0, must not fail regardless
-DROP DATABASE IF NOT EXISTS hbtn_0c_0
+-- deletes database hbtn_0c_0, must not fail regardless
+DROP DATABASE IF NOT EXISTS hbtn_0c_0;
