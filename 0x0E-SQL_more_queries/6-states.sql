8386d81bced2a75a4ff3848c68c1a4cc349ab7eb
root <kaumba@kalichipictures.com>
2024-03-28 14:15:19 -0700

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- creates new database and table in that database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+	name VARCHAR(256)NOT NULL);
