7cc1511518ef445eacf0d6a08d13f91723fc517c
root <kaumba@kalichipictures.com>
2024-04-22 05:35:19 -0700

    task 6

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+// print process.argv
+argv.forEach((val) => {
+	if {
+		val === 0;
+		console.log('No argument');
+	} else if {
+		val === 1;
+		console.log('Argument found');
+	} else {
+		console.log('Arguments found');
+	}
+});
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+compLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let k in compLanguages) {
+	console.log(compLanguages[k]);
+}
+
