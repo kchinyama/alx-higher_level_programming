d455daac14464c9ae3dca2c0132c9fe213c885c2
root <kaumba@kalichipictures.com>
2024-04-22 13:33:09 -0700

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,9 +2,15 @@
 
 function add(a, b) {
   const sum = a + b;
+  console.log(sum);
 }
 
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 
-console.log(add(arg1, arg2));
+if (isNaN(arg1) || isNaN(arg2)) {
+  return;
+} else {
+  add(arg1, arg2);
+}
+
