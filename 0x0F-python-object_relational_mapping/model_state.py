8964cab6ba10d760565109011ef8643be646f211
root <kaumba@kalichipictures.com>
2024-04-28 07:51:32 -0700

    task 6-trim the data

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -22,8 +22,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-
-"""create engine variable that will connect with database at port 3306"""
-#engine = create_engine('mysql://username:password@localhost:3306/database_name', echo=True)
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine, checkfirst=True)
