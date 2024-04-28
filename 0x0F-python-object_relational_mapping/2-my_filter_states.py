2d87a72a3c521e16beba82492b536fd7961e273f
root <kaumba@kalichipictures.com>
2024-04-27 22:03:03 -0700

    task 2-edits on try clause

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,27 +9,30 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    # create connector object that will connect to database
-    connector = MySQLdb.connect(
-            host='localhost', 
-            port=3306, 
-            user=argv[1], 
-            passwd=argv[2],
-            db=argv[3]
-            )
+    try:
 
-    # create cursor object that will interact with the database
-    myCursor = connector.cursor()
+        # create connector object that will connect to database
+        connector = MySQLdb.connect(
+                host='localhost', 
+                port=3306, 
+                user=argv[1], 
+                passwd=argv[2],
+                db=argv[3]
+                )
 
-    # execute the commands required
-    myCursor.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id", (argv[4],))
+        # create cursor object that will interact with the database
+        myCursor = connector.cursor()
 
-    # fetch all the results of the query
-    records = myCursor.fetchall()
+        # execute the commands required
+        myCursor.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id", (argv[4],))
 
-    # iterate through the results in order to print each item
-    for record in records:
-        print(record)
+        # fetch all the results of the query
+        records = myCursor.fetchall()
+
+        # iterate through the results in order to print each item
+        for record in records:
+            print(record)
     
-    # best practuce to close conncetio once done
-    connector.close()
+    finally:
+        # best practuce to close conncetio once done
+        connector.close()
