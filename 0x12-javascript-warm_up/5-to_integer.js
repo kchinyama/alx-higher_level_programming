1525e7109d908d697721edece2ab952a8025081f
kaumba <kaumba@kalichipictures.com>
2024-06-18 14:26:16 -0700

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
 
-if (process.argv[2]) {
-	console.log('My number: ', parseInt(process.argv[2]));
+const k = process.argv[2]
+
+if (process.argv.length === 2) {
+    console.log("Not a number")
+}
+
+else if (isNaN(k)) {
+    console.log("Not a number")
 } else {
-	console.log('Not a number');
+    console.log("My number: " + k)
 }
