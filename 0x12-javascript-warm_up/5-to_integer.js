61529ff84373d8a638b79c529570acb58b07c80b
kaumba <kaumba@kalichipictures.com>
2024-06-18 14:30:16 -0700

    task 5 semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-const k = process.argv[2]
+const k = process.argv[2];
 
 if (process.argv.length === 2) {
-    console.log("Not a number")
-}
-
-else if (isNaN(k)) {
-    console.log("Not a number")
+  console.log('Not a number');
+} else if (isNaN(k)) {
+  console.log('Not a number');
 } else {
-    console.log("My number: " + k)
+  console.log('My number: ' + k);
 }
