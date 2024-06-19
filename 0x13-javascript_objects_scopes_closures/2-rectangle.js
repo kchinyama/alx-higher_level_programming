f64d9f233fe2d7152bdc7265c204bfff7de259c8
kaumba <kaumba@kalichipictures.com>
2024-06-19 14:16:50 -0700

    task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
