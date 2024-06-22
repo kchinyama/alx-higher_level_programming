f9ebe9a8f1a42438fe55a0229fb22cce74a880d1
kaumba <kaumba@kalichipictures.com>
2024-06-22 02:58:58 -0700

    task 0-host and port2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,9 +11,7 @@ if __name__ == '__main__':
 
     myDB = MySQLdb.connect(user=sys.argv[1], 
                         passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        host=localhost,
-                        port=3306
+                        db=sys.argv[3]
                         )
 
     myCur = myDB.cursor()
