a4b5305f9cc93086e3d52b76990fd3dde77be28e
kaumba <kaumba@kalichipictures.com>
2024-06-23 07:05:26 -0700

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+"""sqlalchemy demo of select/querying contents of a table
+"""
+
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == '__main__':
+
+    myEngine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1],
+                                                                            argv[2],
+                                                                            argv[3]))
+
+    Session = sessionmaker(bind=myEngine)
+
+    Base.metadata.create_all(myEngine)
+
+    mySession = Session()
+
+    allStates = mySession.query(State).order_by(State.id).all()
+
+    for state in allStates:
+        print(f"{state.id}. {state.name}")
+
+    mySession.close()
